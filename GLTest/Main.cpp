b7dcbbc489e70bf1b58ef56dba431411bc3c4472
chenjunlong@kingsoft.com <chenjunlong@kingsoft.com>
2020-10-28 09:24:27 +0800

    change to glfw + glad frame

--- GLTest/Main.cpp
+++ GLTest/Main.cpp
@@ -1,41 +1,64 @@
-#include <GLFW/glfw3.h>
+#include <glad\glad.h>
+#include <GLFW\glfw3.h>
 
-int main(void)
+#include <iostream>
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height); 
+void processInput(GLFWwindow* window);
+
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+
+int main()
 {
-    GLFWwindow* window;
+    glfwInit();
 
-    if (!glfwInit())
-        return -1;
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    window = glfwCreateWindow(480, 320, "Hello World", NULL, NULL);
-    if (!window)
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
     {
+        std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
 
     glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
 
     while (!glfwWindowShouldClose(window))
     {
-        glBegin(GL_TRIANGLES);
-
-        glColor3f(1.0, 0.0, 0.0);
-        glVertex3f(0.0, 1.0, 0.0);
-
-        glColor3f(0.0, 1.0, 0.0); 
-        glVertex3f(-1.0, -1.0, 0.0);
-
-        glColor3f(0.0, 0.0, 1.0);
-        glVertex3f(1.0, -1.0, 0.0);
-
-        glEnd();
+        processInput(window);
 
         glfwSwapBuffers(window);
-
         glfwPollEvents();
     }
 
     glfwTerminate();
+
     return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
 }
